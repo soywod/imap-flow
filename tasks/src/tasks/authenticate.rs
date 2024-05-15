fcbdc73e1ad0ce6a31cce91e306812df82e18a23
Clément DOUIN <clement.douin@posteo.net>
2024-05-15 08:37:38 +0200

    feat(imap-tasks): add 2 new mechanisms in `AuthenticateTask`
    Added support for XOAUTH2 and OAUTHBEARER. This last one is not
    supported in imap-types yet, a PR should come soon.

--- tasks/src/tasks/authenticate.rs
+++ tasks/src/tasks/authenticate.rs
@@ -7,6 +7,7 @@ use imap_types::{
     response::{Capability, Code, CommandContinuationRequest, Data, StatusBody, StatusKind},
     secret::Secret,
 };
+use tracing::warn;
 
 use crate::{tasks::TaskError, Task};
 
@@ -14,29 +15,19 @@ use crate::{tasks::TaskError, Task};
 pub struct AuthenticateTask {
     /// Authentication mechanism.
     ///
-    /// Note: Currently only used for `AUTH=PLAIN`.
+    /// Note: Currently only used for `AUTH={PLAIN,XOAUTH2,OAUTHBEARER}`
     ///       Invariants need to be enforced through constructors.
     mechanism: AuthMechanism<'static>,
+
     /// Static authentication data.
     ///
-    /// Note: Currently only used for `AUTH=PLAIN`.
+    /// Note: Currently only used for `AUTH={PLAIN,XOAUTH2,OAUTHBEARER}`
     line: Option<Vec<u8>>,
+
     /// Does the server support SASL's initial response?
     ir: bool,
+
     output: Option<Vec1<Capability<'static>>>,
-}
-
-impl AuthenticateTask {
-    pub fn plain(login: &str, passwd: &str, ir: bool) -> Self {
-        let line = format!("\x00{login}\x00{passwd}");
-
-        Self {
-            mechanism: AuthMechanism::Plain,
-            line: Some(line.into_bytes()),
-            ir,
-            output: None,
-        }
-    }
 }
 
 impl Task for AuthenticateTask {
@@ -67,14 +58,47 @@ impl Task for AuthenticateTask {
 
     fn process_continuation_request_authenticate(
         &mut self,
-        _: CommandContinuationRequest<'static>,
+        continuation: CommandContinuationRequest<'static>,
     ) -> Result<AuthenticateData<'static>, CommandContinuationRequest<'static>> {
-        if self.ir {
-            Ok(AuthenticateData::Cancel)
-        } else if let Some(line) = self.line.take() {
-            Ok(AuthenticateData::r#continue(line))
-        } else {
-            Ok(AuthenticateData::Cancel)
+        match self.mechanism {
+            // See https://developers.google.com/gmail/imap/xoauth2-protocol
+            AuthMechanism::XOAuth2 => {
+                // SASL-IR is supported, so line was already sent.
+                // Therefore, the current continuation request indicates an error.
+                //
+                // > The client sends an empty response ("\r\n") to
+                // > the challenge containing the error message.
+                if self.ir {
+                    match continuation {
+                        CommandContinuationRequest::Basic(basic) => {
+                            let text = basic.text();
+                            warn!("error during XOAUTH2 auth: {text}")
+                        }
+                        CommandContinuationRequest::Base64(data) => {
+                            let text = String::from_utf8_lossy(data.as_ref());
+                            warn!("error during XOAUTH2 auth: {text}")
+                        }
+                    }
+
+                    Ok(AuthenticateData::r#continue(vec![]))
+                } else
+                // SASL-IR is not supported, so the line needs to be sent.
+                if let Some(line) = self.line.take() {
+                    Ok(AuthenticateData::r#continue(line))
+                } else {
+                    Ok(AuthenticateData::Cancel)
+                }
+            }
+            // Default behaviour, may evolve depending on mechanisms.
+            _ => {
+                if self.ir {
+                    Ok(AuthenticateData::Cancel)
+                } else if let Some(line) = self.line.take() {
+                    Ok(AuthenticateData::r#continue(line))
+                } else {
+                    Ok(AuthenticateData::Cancel)
+                }
+            }
         }
     }
 
@@ -90,6 +114,41 @@ impl Task for AuthenticateTask {
             )),
             StatusKind::No => Err(TaskError::UnexpectedNoResponse(status_body)),
             StatusKind::Bad => Err(TaskError::UnexpectedBadResponse(status_body)),
+        }
+    }
+}
+
+impl AuthenticateTask {
+    pub fn plain(login: &str, passwd: &str, ir: bool) -> Self {
+        let line = format!("\x00{login}\x00{passwd}");
+
+        Self {
+            mechanism: AuthMechanism::Plain,
+            line: Some(line.into_bytes()),
+            ir,
+            output: None,
+        }
+    }
+
+    pub fn xoauth2(user: &str, token: &str, ir: bool) -> Self {
+        let line = format!("user={user}\x01auth=Bearer {token}\x01\x01");
+
+        Self {
+            mechanism: AuthMechanism::XOAuth2,
+            line: Some(line.into_bytes()),
+            ir,
+            output: None,
+        }
+    }
+
+    pub fn oauthbearer(a: &str, host: &str, port: u16, token: &str, ir: bool) -> Self {
+        let line = format!("n,a={a},\x01host={host}\x01port={port}\x01auth=Bearer {token}\x01\x01");
+
+        Self {
+            mechanism: AuthMechanism::XOAuth2,
+            line: Some(line.into_bytes()),
+            ir,
+            output: None,
         }
     }
 }
